fb2d58434aa57ce18e4e0e1c781a50516143e82a
lucasmt07 <lucas.mat.ferr.roch@gmail.com>
2026-09-08 22:24:40 -0300

    Ajustes para deploy na cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
+
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://Lucasmath07.github.io',
-  base: '/Portfolio-astro', // O nome exato do seu repositório novo
+  site: 'https://moradanova.pages.dev',
 });
